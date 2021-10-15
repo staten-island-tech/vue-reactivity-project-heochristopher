5a4dff1da7f3fb89fca353459af847745cae8f34
heochristopher <christopher.heo23@sitechhs.com>
2021-10-15 12:28:02 -0400

    assets added

--- btd/src/App.vue
+++ btd/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/monkey-meadow.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/maps/chutes.png">
+    <HelloWorld msg="Vloons Tower Defense"/>
   </div>
 </template>
 
